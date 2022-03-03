c0a01bce180da26e1c51a2fd5d4218a40fe49bdd
Douglas Borges <douglasborges19@gmail.com>
2022-03-02 23:45:38 -0300

    define layout da pagina index.php

--- index.php
+++ index.php
@@ -1,20 +1,42 @@
-<!DOCTYPE html>
-<html>
+<?php
+include_once 'includes/header.php';
+?>
+<div class="row">
+  <div class="col s12 m6 push-m3">
+    <h3 class="light">Clientes</h3>
+    <table class="striped">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+          <th>Email</th>
+          <th>Idade</th>
+        </tr>
+      </thead>
 
-<head>
-  <!--Import Google Icon Font-->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!--Import materialize.css-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-  <!--Let browser know website is optimized for mobile-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</head>
-
-<body>
-
-  <!--JavaScript at end of body for optimized loading-->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</body>
-
-</html>
+      <tbody>
+        <tr>
+          <td>Douglas</td>
+          <td>Borges</td>
+          <td>douglas@furg.br</td>
+          <td>28</td>
+          <td>
+            <a href="" class="btn-floating orange">
+              <i class="material-icons">edit</i>
+            </a>
+          </td>
+          <td>
+            <a href="" class="btn-floating red">
+              <i class="material-icons">delete</i>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <a href="" class="btn">Adicionar cliente</a>
+  </div>
+</div>
+<?php
+include_once 'includes/footer.php';
+?>
